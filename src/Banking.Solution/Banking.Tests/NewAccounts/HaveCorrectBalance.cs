da74457d702143ca51135b558ecd5693c11300d3
Waterlily127 <crystal.mat11@gmail.com>
2025-09-09 20:50:33 +0000

    Setting up Banking proj

---
+++ src/Banking.Solution/Banking.Tests/NewAccounts/HaveCorrectBalance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking.Domain;
+
+namespace Banking.Tests.NewAccounts;
+public class HaveCorrectBalance
+{
+
+    [Fact]
+    public void CorrectBalanceForNewAccounts()
+    {
+        // given I have a bank account
+        var account = new BankAccount();
+        // when I ask that account for the balance
+        decimal balance = account.GetBalance();
+
+        // then it should be...
+        Assert.Equal(5000M, balance);
+
+
+    }
+}
