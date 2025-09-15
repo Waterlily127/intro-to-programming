043b08d405052e760496cb2ce577d533f72d6d9f
Waterlily127 <crystal.mat11@gmail.com>
2025-09-15 20:44:37 +0000

    day 06 work

---
+++ src/Links/frontend/src/app/components/nav-link.ts
@@ -0,0 +1,22 @@
+import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { NavLink } from './types';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-link',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink, RouterLinkActive],
+  template: `
+    <a
+      class="link px-2 "
+      [routerLinkActive]="['font-bold', 'text-accent']"
+      [routerLinkActiveOptions]="{ exact: true }"
+      [routerLink]="link().href"
+      >{{ link().label }}
+    </a>
+  `,
+  styles: ``,
+})
+export class NavBarLink {
+  link = input.required<NavLink>();
+}
---
+++ src/Links/frontend/src/app/pages/support.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ``,
+  styles: ``,
+})
+export class Support {}
